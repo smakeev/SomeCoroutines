f1ce3c75c455d95a104523ab4b9a35cf5bd85be8
Sergey Makeev <makeev@freeconferencecall.com>
2019-08-14 15:57:42 +0300

    generators range and comprehence - ready.

--- SomeCoroutine/SomeCoroutine/SomeCoroutine.swift
+++ SomeCoroutine/SomeCoroutine/SomeCoroutine.swift
@@ -13,52 +13,145 @@ public class SomeGenerator<Type> {
 	public internal(set) var finished: Bool = false
 	public internal(set) var current: Type? = nil
 
-	public static func fromArray(_ array: [Type]) -> SomeGenerator {
-		let result = SomeGenerator<Type>()
-		result.arrayBuilder = array
+	public static func fromArray(_ array: [Type], additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
+		return SomeGenerator.comprehension({array[$0]}, iterations: array.count, additional: additional)
+	}
+
+	public static func comprehension(_ action: @escaping ((Int) -> Type?), iterations: Int, additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
+		let result = SomeGenerator<Type>.comprehension(action, isFinished: { (iteration, _) -> Bool in
+			return iteration >= iterations - 1
+		}, additional: additional)
 		return result
 	}
 
-	public static func comprehension(_ action: @escaping ((Int) -> Type?), iterations: Int) -> SomeGenerator {
-		let result = SomeGenerator<Type>.comprehension(action) { (iteration, _) -> Bool in
-			return iteration >= iterations
+	public static func comprehension(_ action: @escaping ((Int) -> Type?), isFinished: @escaping ((Int, Type?)->Bool), additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
+		let result = SomeGenerator<Type>()
+		result.isFinishedBlock = isFinished
+		result.actionBuilder   = action
+		result.additionalAction = additional
+		return result
+	}
+
+	/*
+		returns array from generator,
+		generator produces values just the same way as using next method.
+		At the end you will have an array of all elements and genrator will e finished.
+		If you have a generator without finish element, don't use this method.
+	*/
+	public func toArray() -> Array<Type> {
+		var array = [Type]()
+		while !finished {
+			if let value = internalNext() {
+				array.append(value)
+			}
 		}
-		return result
+		return array
 	}
 
-	public static func comprehension(_ action: @escaping ((Int) -> Type?), isFinished: @escaping ((Int, Type?)->Bool)) -> SomeGenerator {
-		let result = SomeGenerator<Type>()
+	/*
+	Provides next generator's step.
+	If generator is finished will return nil
+	*/
+	public func next() -> Type? {
+		return next(nil)
+	}
 
+	/*
+	Same as next, but with additional new value.
+	Value is an additional argument wich will be provided to the generator on next iteration.
+	If additionalAction has been provided it will be called with this value
+	For range generator if no additional action step will be changed on new value.
+	Note, step changed only for one iteration.
+	Step should be in right direction. Otherwise generator will be finished.
+	For comprehension: if additionalActian has been provided it will be called with this value.
+	If no additional action - just ignore.
+	For YIELD generator, current yield call will return this value.
+	There is no additional action for YIELD generators.
+	*/
+	public func next(_ value: Any?) -> Type? {
+		additionalParam = value
+		if rangeBuilder != nil ||
+		   actionBuilder != nil {
+				return internalNext()
+		}
 
-
-		return result
+		return nil
 	}
 
 	internal var rangeBuilder: ((_: SomeGenerator<Type>) -> Type?)? = nil
-	internal var arrayBuilder: [Type]? = nil
+	internal var actionBuilder: ((Int) -> Type?)? = nil
+	internal var isFinishedBlock: ((Int, Type?) -> Bool)? = nil
+	internal var additionalAction: ((Type, Any) -> Type)? = nil
 
-	private var iterationNumber: Int = 0
+	internal var iterationNumber: Int = 0
+	internal var additionalParam: Any? = nil
+
+	fileprivate func internalNext() -> Type? {
+		guard !finished else { return nil }
+		if let rangeGenerator = rangeBuilder {
+			let value = rangeGenerator(self)
+			if let validValue = value {
+				current = validValue
+				iterationNumber += 1
+				return current
+			} else {
+				finished = true
+			}
+		} else if let actionGenerator = actionBuilder, let isFinished = isFinishedBlock {
+			let result = actionGenerator(iterationNumber)
+			if let validResult = result {
+				if let additionalAction = additionalAction, let additionalParam = additionalParam {
+					let newResult = additionalAction(validResult, additionalParam)
+					current = newResult
+				} else {
+					current = validResult
+				}
+			}
+			additionalParam = nil
+			if isFinished(iterationNumber, result) {
+				finished = true
+			}
+			iterationNumber += 1
+			return current
+		}
+
+		return nil
+	}
+
+	fileprivate func yield(_ whatToReturn: Type) -> Any? {
+		return nil
+	}
 }
 
-extension SomeGenerator where Type: Numeric & Comparable {
+public extension SomeGenerator where Type: Numeric & Comparable {
 
-	public static func range(_ target: Type) -> SomeGenerator {
+	static func range(_ target: Type, additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
 		return SomeGenerator.range(nil, target)
 	}
 
-	public static func range(_ target: Type, step: Type) -> SomeGenerator {
-		return SomeGenerator.range(nil, target, step)
+	static func range(_ target: Type, step: Type, additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
+		return SomeGenerator.range(nil, target, step, additional: additional)
 	}
 
-	public static func range(_ begin: Type?, _ target: Type, _ step: Type? = nil) -> SomeGenerator {
+	static func range(_ begin: Type?, _ target: Type, _ step: Type? = nil, additional: ((Type, Any) -> Type)? = nil) -> SomeGenerator {
 		let result = SomeGenerator<Type>()
+		result.additionalAction = additional
 		result.rangeBuilder = { generator in
 			let validBegin = generator.current ?? begin ?? Type.zero
-			guard let validStep = step ?? Type(exactly: target > validBegin ? 1 : -1) else { return nil }
+			guard var validStep = step ?? Type(exactly: target > validBegin ? 1 : -1) else { return nil }
+			//check for additional
+			if let additionalParam = generator.additionalParam as? Type {
+				if generator.additionalAction == nil {
+					validStep = additionalParam
+				}
+			}
 			guard validStep != 0 else { return nil }
 
-			let nextInRange = generator.current == nil ? validBegin :validBegin + validStep
-
+			var nextInRange = generator.current == nil ? validBegin : validBegin + validStep
+			if let additionalAction = generator.additionalAction, let additionalParam = generator.additionalParam {
+				nextInRange = additionalAction(nextInRange, additionalParam)
+			}
+			generator.additionalParam = nil
 			if validStep > 0 {
 				if nextInRange >= target {
 					return nil
@@ -72,6 +165,29 @@ extension SomeGenerator where Type: Numeric & Comparable {
 			return nextInRange
 		}
 		return result
+	}
+}
+
+extension SomeGenerator: Sequence {
+	public typealias Element = Type
+	public typealias Iterator = GeneratorIterator<Type>
+
+	public __consuming func makeIterator() -> Iterator {
+		return GeneratorIterator<Type>(generator: self)
+	}
+}
+
+public struct GeneratorIterator<Type>: IteratorProtocol {
+	public typealias Element = Type
+
+	var generator: SomeGenerator<Type>
+
+	init(generator: SomeGenerator<Type>) {
+		self.generator = generator
+	}
+
+	mutating public func next() -> Type? {
+		return generator.internalNext()
 	}
 }
 
--- SomeCoroutine/Tests/Tests.swift
+++ SomeCoroutine/Tests/Tests.swift
@@ -20,6 +20,23 @@ extension SomeGenerator {
 		}
 		return resultArray
 	}
+
+	public func testComprehension() -> [Type] {
+		guard let actionBuilder = actionBuilder else { return [] }
+		guard let isFinishedBlock = isFinishedBlock else { return [] }
+		var resultArray = [Type]()
+
+		while !finished {
+			if let result = actionBuilder(iterationNumber) {
+				resultArray.append(result)
+				if isFinishedBlock(iterationNumber, result) {
+					finished = true
+				}
+			}
+			iterationNumber += 1
+		}
+		return resultArray
+	}
 }
 
 
@@ -54,17 +71,123 @@ class Tests: XCTestCase {
 		XCTAssert(rangeTargetStep.testRangeCreationRun() == [0, 3, 6, 9])
 		let rangeWrongTargetStep = SomeGenerator.range(10, step: -3)
 		XCTAssert(rangeWrongTargetStep.testRangeCreationRun() == [])
+		let fromNegative = SomeGenerator.range(-3, 2)
+		XCTAssert(fromNegative.testRangeCreationRun() == [-3, -2, -1, 0, 1])
+		let fromNegativeToNegative = SomeGenerator.range(-3, -10)
+		XCTAssert(fromNegativeToNegative.testRangeCreationRun() == [-3, -4, -5, -6, -7, -8, -9])
+		let fromNegativeToNegativeithStep = SomeGenerator.range(-3, -10, -2)
+		XCTAssert(fromNegativeToNegativeithStep.testRangeCreationRun() == [-3, -5, -7, -9])
 	}
 
 	func testComprehansion() {
 		let array = [0, 1, 2, 3 , 4, 5]
+		let fromArray = SG.fromArray(array)
+		XCTAssert(fromArray.testComprehension() == [0, 1, 2, 3 , 4, 5])
 		let comprehension = SG.comprehension({array[$0] * array[$0]}, iterations: array.count)
-
+		XCTAssert(comprehension.testComprehension() == [0, 1, 4, 9 , 16, 25])
 		//only for odd
-		let comprehensionOdd = SG.comprehension({
-				array[$0] * array[$0]
+		let comprehensionOdd = SG<Int>.comprehension({ 
+			guard $0 % 2 != 0 else { return nil }
+			return array[$0] * array[$0]
 
 		}, iterations: array.count)
+		XCTAssert(comprehensionOdd.testComprehension() == [1, 9, 25])
+	}
+
+	func testToArray() {
+		let range = SomeGenerator.range(0, 10, 1)
+		XCTAssert(range.toArray() == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+		let comprehension = SG.comprehension({$0}, iterations: 5)
+		XCTAssert(comprehension.toArray() == [0, 1, 2, 3, 4])
+		let arrayOfArrays = SG.comprehension({SG.comprehension({$0}, iterations: ($0 + 1)).toArray()}, iterations: 5)
+		XCTAssert(arrayOfArrays.toArray() == [[0],
+											  [0, 1],
+										      [0, 1, 2],
+										      [0, 1, 2, 3],
+											  [0, 1, 2, 3, 4]])
+	}
+
+	func testNext() {
+		let range = SomeGenerator.range(0, 5, 1)
+		XCTAssert(range.current == nil)
+		XCTAssert(range.next() == 0)
+		XCTAssert(range.current == 0)
+		XCTAssert(range.next() == 1)
+		XCTAssert(range.current == 1)
+		XCTAssert(range.next() == 2)
+		XCTAssert(range.current == 2)
+		XCTAssert(range.next() == 3)
+		XCTAssert(range.current == 3)
+		XCTAssert(range.next() == 4)
+		XCTAssert(range.current == 4)
+		XCTAssert(range.next() == nil)
+		XCTAssert(range.next() == nil)
+		XCTAssert(range.current == 4)
+
+		let comprehension = SG.comprehension({$0}, isFinished:{ iteration, _ in
+			return iteration == 2
+		} )
+		XCTAssert(comprehension.current == nil)
+		XCTAssert(comprehension.next() == 0)
+		XCTAssert(comprehension.next() == 1)
+		XCTAssert(comprehension.next() == 2)
+		XCTAssert(comprehension.next() == nil)
+		XCTAssert(comprehension.next() == nil)
+		XCTAssert(comprehension.current == 2)
+
+	}
+
+	func testForIn() {
+		var result = [String]()
+		let input  = ["first", "second", "third"]
+		let fromArray = SG.fromArray(input)
+		for str in fromArray {
+			result.append(str)
+		}
+		XCTAssert(result == input)
+		for str in fromArray {
+			result.append(str)
+		}
+		XCTAssert(result == input)
+	}
+
+	func testAdditionalParam() {
+		let range = SomeGenerator.range(0, 5, 1)
+		XCTAssert(range.next()  == 0)
+		XCTAssert(range.next(2) == 2)
+		XCTAssert(range.next(-1) == nil)
+		XCTAssert(range.next() == nil)
+		XCTAssert(range.current == 2)
+
+		let range2 = SomeGenerator.range(0, 5, 1)
+		XCTAssert(range2.next(2) == 0)
+		XCTAssert(range2.next() == 1)
+
+		let randeAdditional = SomeGenerator.range(10, step: 1) {  result, add  in
+			if let validAdd = add as? Int {
+				return result + validAdd
+			} else {
+				return result
+			}
+		}
+
+		XCTAssert(randeAdditional.next(1) == 1)
+		XCTAssert(randeAdditional.next(2) == 4)
+		XCTAssert(randeAdditional.next(12) == nil)
+		XCTAssert(randeAdditional.current == 4)
+
+
+		let com = SomeGenerator.comprehension({$0}, isFinished: { iter, _ in iter == 10 }, additional: { value, addition in
+			if let validAddition = addition as? Int {
+				return value + validAddition
+			} else {
+				return value
+			}
+		})
+
+		XCTAssert(com.next(1) == 1)
+		XCTAssert(com.next(-1) == 0)
+		XCTAssert(com.toArray() == [2, 3, 4, 5, 6, 7, 8, 9, 10])
 	}
 
     func testPerformanceExample() {
